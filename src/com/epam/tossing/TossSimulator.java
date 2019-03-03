a070e550e76f171d38d44624326a0240b5d074a6
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-03-03 17:50:03 +0300

    Release

--- src/com/epam/tossing/TossSimulator.java
+++ src/com/epam/tossing/TossSimulator.java
@@ -6,7 +6,6 @@ public class TossSimulator {
 	//-----------------------------------------------------------------------------fields
 	private int headsCount;
 	private int tailsCount;
-	private int tossesCounter;
 
 	public static final int TOSSBOUND = 99999;
 	//-----------------------------------------------------------------------------constructors
@@ -14,27 +13,24 @@ public class TossSimulator {
 	{
 		this.headsCount = 0;
 		this.tailsCount = 0;
-		this.tossesCounter = 0;
 	}
 	//-----------------------------------------------------------------------------
 	//-----------------------------------------------------------------------------methods
 	public void startToss(int tossesNumber)
 	{
 		SecureRandom randomNumber = new SecureRandom();
-		while(tossesNumber > this.tossesCounter++) {
-			if((randomNumber.nextInt() % 2) != 0) {
+		for(int i = 0; i < tossesNumber; i++) {
+			if(randomNumber.nextBoolean()) {
 				this.headsCount++;
-			}else {
-				this.tailsCount++;
 			}
-		 }
+		}
+		this.tailsCount = tossesNumber - this.headsCount;
 	}
 
 	public void reset()
 	{
 		this.headsCount = 0;
 		this.tailsCount = 0;
-		this.tossesCounter = 0;
 	}
 	//-----------------------------------------------------------------------------getters
 	public int getHeadsCount() {
